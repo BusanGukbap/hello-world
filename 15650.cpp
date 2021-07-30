02ad69a8ee0cb3619eeda117afb4a11b9f873674
김동윤 <dong2857210@gmail.com>
2021-07-30 21:15:34 +0900

    N과 M (2) (15650)

---
+++ 15650.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int arr[10]; // 수열을 저장하는 곳
+bool visit[10]; // visit[i] = 0일땐 arr에 i가 없음, visit[i] = 1일땐 arr에 i가 있음
+
+// index = arr에 담겨있는 수의 개수
+void func(int index) {
+	if (index == M+1) {
+		for (int i = 1; i <= M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = arr[index-1]+1; i <= N; i++) {
+		if (!visit[i]) {
+			arr[index] = i;
+
+			visit[i] = 1;
+			func(index + 1);
+			visit[i] = 0;
+		}
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+
+	func(1);
+
+	return 0;
+}
