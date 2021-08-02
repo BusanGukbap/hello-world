36ba995ac8b4511bfa17cb9507d4e6ade395916e
김동윤 <dong2857210@gmail.com>
2021-08-03 00:43:41 +0900

    N과 M (11) (15665)

---
+++ 15665.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int input[9];
+int save[9];
+
+void func(int index) {
+	if (index == M) {
+		for (int i = 0; i < M; i++) {
+			cout << save[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	int before = -1;
+	for (int i = 0; i < N; i++) {
+		if (before != input[i]) {
+			before = input[i];
+			save[index] = input[i];
+			func(index + 1);
+		}
+	}
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> input[i];
+	}
+	sort(input, input + N);
+
+	func(0);
+
+	return 0;
+}
