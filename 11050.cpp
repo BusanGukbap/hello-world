f243ae464baf64d869008df8487c1fb12fd099dd
김동윤 <dong2857210@gmail.com>
2021-09-14 22:49:17 +0900

    이항 계수 1 (11050)

---
+++ 11050.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int arr[11][11];
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N, K;
+	cin >> N >> K;
+	
+	for (int i = 0; i <= N; i++) {
+		for (int j = 0; j <= i; j++) {
+			if (j == 0 || j == i)
+				arr[i][j] = 1;
+			else
+				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+		}
+	}
+
+	cout << arr[N][K];
+
+	return 0;
+}
