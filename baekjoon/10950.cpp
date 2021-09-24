a3ff30ec3ac5ca5a78e727514bd0de22e9951801
김동윤 <dong2857210@gmail.com>
2021-09-24 23:58:46 +0900

    A+B - 3 (10950)
    문제이름 : A+B - 3
    문제번호 : 10950
    난이도 : 브론즈3
    시간제한 : 1초
    메모리제한 : 256MB
    알고리즘 분류 : 수학, 구현, 사칙연산

---
+++ baekjoon/10950.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+	int A, B;
+	for (int test_case = 0; test_case < T; test_case++) {
+		cin >> A >> B;
+		cout << A + B << '\n';
+	}
+
+
+	return 0;
+}
