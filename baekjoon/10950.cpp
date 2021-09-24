a1dc1598d59f27153568ef6dfca5fce63cd7df27
BusanGukbap <77170214+BusanGukbap@users.noreply.github.com>
2021-09-25 00:06:28 +0900

    Update 10950.cpp
    문제이름 : A+B - 3
    문제번호 : 10950
    난이도 : 브론즈3
    시간제한 : 1초
    메모리제한 : 256MB
    알고리즘 분류 : 수학, 구현, 사칙연산
    https://www.acmicpc.net/problem/10950

--- baekjoon/10950.cpp
+++ baekjoon/10950.cpp
@@ -8,9 +8,9 @@ int main(void) {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int T;
+	int T, A, B;
 	cin >> T;
-	int A, B;
+	
 	for (int test_case = 0; test_case < T; test_case++) {
 		cin >> A >> B;
 		cout << A + B << '\n';
