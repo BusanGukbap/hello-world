3868821c91c8735961be8ae29420f03ad82f5371
DongYoon Kim <dong2857210@gmail.com>
2022-10-26 20:55:06 +0900

    계란으로 계란치기 (16987)

--- baekjoon/16987_계란으로 계란치기.cpp
+++ baekjoon/16987_계란으로 계란치기.cpp
@@ -1,62 +1,72 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int N;
-int result;
+int N, answer;
 int S[9];
 int W[9];
+int egg;
 
-void func(int index) {
+void input() {
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> S[i] >> W[i];
+	}
+}
+
+void backtracking(int index) {
+	// 다 돌았을때
 	if (index > N) {
 		int cnt = 0;
 		for (int i = 1; i <= N; i++) {
 			if (S[i] <= 0)
 				cnt++;
 		}
-		if (cnt > result) 
-			result = cnt;
-		
+
+		if (cnt > answer)
+			answer = cnt;
 		return;
 	}
 
-	if (S[index] <= 0)
-		func(index + 1);
+	if (S[index] <= 0) {
+		backtracking(index + 1);
+	}
 
 	else {
 		int cnt = 0;
 		for (int i = 1; i <= N; i++) {
-			if (S[i] > 0 && index != i) {
-				S[i] -= W[index];
-				S[index] -= W[i];
-				cnt++;
-				func(index + 1);
-				S[i] += W[index];
-				S[index] += W[i];
-			}
+			// 손에 들고 있는 계란이거나 계란이 부서진경우 넘어감
+			if (i == index)
+				continue;
+			if (S[i] <= 0)
+				continue;
+			
+			// 손에 든 계란으로 계란 때리기
+			S[index] -= W[i];
+			S[i] -= W[index];
+			cnt++;
+			backtracking(index + 1);
+			S[index] += W[i];
+			S[i] += W[index];
 		}
-		if (cnt == 0) {
-			func(N + 1);
-		}
+
+		if (cnt == 0)
+			backtracking(index + 1);
 	}
 }
 
-int main(void) {
+void solution() {
+	input();
+	backtracking(1);
+	cout << answer << '\n';
+}
+
+int main() {
 	ios_base::sync_with_stdio(0);
+	cout.tie(0);
 	cin.tie(0);
 
-	cin >> N;
-	result = 0;
-	for (int i = 1; i <= N; i++) {
-		cin >> S[i] >> W[i];
-	}
-
-	func(1);
-
-	cout << result;
-
+	solution();
 
 	return 0;
 }
