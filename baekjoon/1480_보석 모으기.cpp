9b538a83da745d6461cadc143afba5a846a89f36
DongYoon Kim <dong2857210@gmail.com>
2022-10-26 20:58:58 +0900

    보석 모으기 (1480)

---
+++ baekjoon/1480_보석 모으기.cpp
@@ -0,0 +1,28 @@
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int N, M, C;
+//vector<int> gem;
+//int dp[12][1 << 13][22]; // [가방][보석][한도]
+//
+//int solve(int cur, int visited, int capacity) {
+//
+//}
+//
+//int main() {
+//	ios_base::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//
+//	int temp;
+//	cin >> N >> M >> C;
+//	for (int i = 0; i < N; i++) {
+//		cin >> temp;
+//		gem.push_back(temp);
+//	}
+//
+//	cout << solve(0, 0, C);
+//
+//	return 0;
+//}
